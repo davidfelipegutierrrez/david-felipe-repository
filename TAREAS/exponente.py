0ca64736ff551c793d9878a0a65c3e28d380db55
david-felipe-gutierrez <141975325+davidfelipegutierrrez@users.noreply.github.com>
2023-09-19 23:49:15 -0500

    Add files via upload

---
+++ TAREAS/exponente.py
@@ -0,0 +1,29 @@
+# m = int(input("ingrese el valor del exponente: "))
+# n = int(input("ingrese la base de la base: "))
+# def exponentes(m,n):
+#     if m == 0:
+#         return 1
+#     else:
+#        return n*exponentes(m-1,n)
+# resultado = exponentes(m,n)
+# print(resultado)
+
+# X = int(input("valor de la base:"))
+# Y = int(input("valor del exponente:"))
+# def exponentes(X,Y):
+#     if Y == 0:
+#         return 1
+#     else:
+#         return X*exponentes(X,Y-1)
+# resultado = exponentes(X,Y)
+# print(resultado)
+
+# N = int(input("defina el valor: "))
+# def faactorial(N):
+#     if N == 0:
+#         return 1
+#     else:
+#         return N*faactorial(N-1)
+# resultado = faactorial(N)
+# print(resultado)
+
